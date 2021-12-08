8ba3e602ed7a537450834a33b39268177dfda9ec
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-12-07 23:20:06 -0500

    Delivery status update trigger

--- triggers.sql
+++ triggers.sql
@@ -14,3 +14,11 @@ BEGIN
     UPDATE product SET product.quantity = product.quantity + :old.quantity
     where product.product_id = :old.product_id;
 END;
+
+/* Update delivery date to delivered date on orders when delivery status is set to delivered on tracking */
+CREATE OR REPLACE TRIGGER DELIVERYDATEUPDATE
+AFTER UPDATE ON ORDER_TRACKING 
+FOR EACH ROW
+BEGIN
+    UPDATE orders SET orders.delivery_datetime = SYSDATE where orders.order_id = :new.order_id and :new.delivery_status = 'DELIVERED';
+END;
