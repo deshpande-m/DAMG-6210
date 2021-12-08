aabaa903c31624daf2df4473a6bb5c08eeda0929
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-12-07 22:33:54 -0500

    Delete product quantity trigger updated

--- triggers.sql
+++ triggers.sql
@@ -1,7 +1,16 @@
+/* Decrease inventory on placing an order */
 CREATE OR REPLACE TRIGGER QUANTITYUPDATE 
 AFTER INSERT ON ORDER_ITEMS 
 FOR EACH ROW
 BEGIN
     UPDATE product SET product.quantity = product.quantity - :new.quantity
     where product.product_id = :new.product_id;
+END;
+
+/* Increase inventory on canceling an order */
+CREATE OR REPLACE TRIGGER INCREASEQUANTITYUPDATE  BEFORE DELETE ON ORDER_ITEMS
+FOR EACH ROW
+BEGIN
+    UPDATE product SET product.quantity = product.quantity + :old.quantity
+    where product.product_id = :old.product_id;
 END;
