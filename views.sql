aca930f07c74c780e65bf5c012a9c3bc4dd84675
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-12-10 15:56:21 -0500

    Select statements for views added to run scripts

--- views.sql
+++ views.sql
@@ -6,6 +6,8 @@ join orders on orders.order_id = o.order_id
 group by p.product_name,orders.order_date,o.quantity
 order by 3 desc,2 desc;
 
+select * from top_products;
+
 -- View to get top 3 categories of product sold by quantity
 Create or replace view Top_Categories as
 select c.category_name, p.product_name, sum(o.quantity) as "Total Quantity Sold" from Category c
@@ -13,9 +15,13 @@ join product p on c.category_id = p.product_id
 join order_items o on o.product_id = p.product_id
 group by c.category_name,p.product_name;
 
+select * from top_categories;
+
 --View to get inventory status and manufacture report to view products low on stock
 CREATE OR REPLACE VIEW Inventory_status as
 SELECT p.product_id,p.product_name,p.quantity as "Inventory",c.category_name,m.manufacturer_name from product p
 inner join category c on p.category_id = c.category_id
 inner join manufacturer m on p.manufacturer_id = m.manufacturer_id
 order by p.quantity asc;
+
+Select * from inventory_status;
