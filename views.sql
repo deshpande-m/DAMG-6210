d67773cfeb9be0aa63991d16d9293a2347f1f12d
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-12-10 15:53:16 -0500

    View to get top 3 categories of product sold by quantity

--- views.sql
+++ views.sql
@@ -5,3 +5,10 @@ join order_items o on p.product_id = o.product_id
 join orders on orders.order_id = o.order_id
 group by p.product_name,orders.order_date,o.quantity
 order by 3 desc,2 desc;
+
+-- View to get top 3 categories of product sold by quantity
+Create or replace view Top_Categories as
+select c.category_name, p.product_name, sum(o.quantity) as "Total Quantity Sold" from Category c
+join product p on c.category_id = p.product_id
+join order_items o on o.product_id = p.product_id
+group by c.category_name,p.product_name;
