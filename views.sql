62d09d0971e4fc5cc2fca34bc4f00b32ebc39d8a
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-12-11 20:42:49 -0500

    Trigger changes

--- views.sql
+++ views.sql
@@ -1,19 +1,25 @@
 -- View to get top 3 products sold by quantity per year
-create or replace view Top_Products as
-select p.product_name, sum(o.quantity) as "Total Quantity Sold", EXTRACT(YEAR from (orders.order_date)) as "Year"  from product p
+select product_name, "Total Quantity Sold", "Year" from
+(select p.product_name, sum(o.quantity) as "Total Quantity Sold", EXTRACT(YEAR from (orders.order_date)) as "Year",
+rank() over (partition by EXTRACT(YEAR from (orders.order_date)) order by sum(o.quantity) desc) as rn
+from product p
 join order_items o on p.product_id = o.product_id
 join orders on orders.order_id = o.order_id
-group by p.product_name,orders.order_date,o.quantity
-order by 3 desc,2 desc;
+group by p.product_name,orders.order_date,o.quantity) a
+where a. rn <= 3;
 
 select * from top_products;
 
 -- View to get top 3 categories of product sold by quantity
 Create or replace view Top_Categories as
-select c.category_name, p.product_name, sum(o.quantity) as "Total Quantity Sold" from Category c
-join product p on c.category_id = p.product_id
+select a.category_name, a.product_name, a.prodcount from
+(select c.category_name, p.product_name, sum(o.quantity) as prodcount,
+rank() over(partition by c.category_name order by sum(o.quantity) desc) as rn from Category c 
+left join product p
+on c.category_id = p.category_id
 join order_items o on o.product_id = p.product_id
-group by c.category_name,p.product_name;
+group by c.category_name, p.product_name) a
+where rn in (1,2,3);
 
 select * from top_categories;
 
@@ -25,3 +31,4 @@ inner join manufacturer m on p.manufacturer_id = m.manufacturer_id
 order by p.quantity asc;
 
 Select * from inventory_status;
+
