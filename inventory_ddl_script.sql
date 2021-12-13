e1835a086974e8b8c4cf1363c9818f201a2124e2
Mayank Deshpande <deshpande.m@northeastern.edu>
2021-12-13 02:26:19 -0500

    adding triggers to the ddl script

--- inventory_ddl_script.sql
+++ inventory_ddl_script.sql
@@ -1327,4 +1327,33 @@ CREATE OR REPLACE PACKAGE BODY inventory_utils AS
             
     END insert_reviews;
     
-END inventory_utils; 
+END inventory_utils;
+/
+
+/* Trigger to Decrease inventory on placing an order */
+CREATE OR REPLACE TRIGGER QUANTITYUPDATE 
+AFTER INSERT ON ORDER_ITEMS 
+FOR EACH ROW
+BEGIN
+    UPDATE product SET product.quantity = product.quantity - :new.quantity
+    where product.product_id = :new.product_id;
+END;
+/
+
+/* Trigger to Increase inventory on canceling an order */
+CREATE OR REPLACE TRIGGER INCREASEQUANTITYUPDATE  BEFORE DELETE ON ORDER_ITEMS
+FOR EACH ROW
+BEGIN
+    UPDATE product SET product.quantity = product.quantity + :old.quantity
+    where product.product_id = :old.product_id;
+END;
+/
+
+/* Trigger to Update delivery date to delivered date on orders when delivery status is set to delivered on tracking */
+CREATE OR REPLACE TRIGGER DELIVERYDATEUPDATE
+AFTER UPDATE ON ORDER_TRACKING 
+FOR EACH ROW
+BEGIN
+    UPDATE orders SET orders.delivery_datetime = SYSDATE where orders.order_id = :new.order_id and :new.delivery_status = 'DELIVERED';
+END;
+/
